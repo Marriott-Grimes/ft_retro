75a458c55011882e28b3582d4cbe43aa224e3a82
Artem Kalmykov <kalmykov.artem@gmail.com>
2017-07-08 12:36:31 -0700

    ncurses lib

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: akalmyko <akalmyko@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/07/08 12:07:14 by akalmyko          #+#    #+#              #
-#    Updated: 2017/07/08 12:11:02 by akalmyko         ###   ########.fr        #
+#    Updated: 2017/07/08 12:35:53 by akalmyko         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,11 +17,12 @@ CPPFILES = main.cpp
 FLAGS = -Wall -Wextra -Werror
 COMPILER = clang++
 RMU = /bin/rm -f
+# INC = -I /nfs/2016/a/akalmyko/goinfre/brew/opt/ncurses/lib -I /nfs/2016/a/akalmyko/goinfre/brew/opt/ncurses/include
 
 all: $(NAME)
 
 $(NAME):
-	@$(COMPILER) $(FLAGS) $(CPPFILES) -o $(NAME)
+	@$(COMPILER) $(FLAGS) $(CPPFILES) -o $(NAME) -lncurses
 
 clean:
 	@echo 'nothing to clean'
--- main.cpp
+++ main.cpp
@@ -6,12 +6,22 @@
 /*   By: akalmyko <akalmyko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 12:07:18 by akalmyko          #+#    #+#             */
-/*   Updated: 2017/07/08 12:08:06 by akalmyko         ###   ########.fr       */
+/*   Updated: 2017/07/08 12:35:35 by akalmyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <ncurses.h>
+// #include <unistd.h>
+// #include <cstdlib>
+// #include <ctime>
+// #include <signal.h>
 
 int main(void) {
-	return (0);
+	initscr();                 /* Start curses mode     */
+    printw("Hello World !!!"); /* Print Hello World    */
+    refresh();                 /* Print it on to the real screen */
+    getch();                   /* Wait for user input */
+    endwin();                  /* End curses mode    */
+    return (0);
 }
